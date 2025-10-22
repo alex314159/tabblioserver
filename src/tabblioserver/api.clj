9161f1dc9a5a9c682c49e0045c071b81eb4e6526
Alexandre Almosni <alexandre.almosni@gmail.com>
2025-10-22 22:47:44 +0100

    api save template

--- src/tabblioserver/api.clj
+++ src/tabblioserver/api.clj
@@ -30,16 +30,17 @@
       (-> (response {:error "Authentication required"})
           (status 401)))))
 
+(def last-request (atom nil))
+
 (defn save-template [request]
-  (let [user (:user request)
-        template-data (:body request)
-        user-id (:user-id user)
-        enhanced-data (assoc template-data :username user-id)]
-    (if user-id
-      (let [template-id (sql/save-template enhanced-data)]
-        (response {:success true :template-id template-id}))
-      (-> (response {:error "Authentication required"})
-          (status 401)))))
+  (reset! last-request request)
+  (let [                                                    ;user (:user request)
+        template-data (cheshire.core/parse-string (:body request) true)
+        ;user-id (:user-id user)
+        user-id nil
+        enhanced-data (assoc template-data :username (or user-id "") :nickname "")]
+    (sql/save-template enhanced-data)
+    (response {:success true :uuid (:uuid template-data)})))
 
 (defn load-template [request]
   (let [template-id (get-in request [:body :uuid])
@@ -140,27 +141,29 @@
 
 (def routes
   [["/" {:get {:handler (fn [_] (response {:message "TabblioServer API"}))}}]
-   ["/save-template" {:post {:handler (require-auth save-template)}}]
-   ["/load-template" {:post {:handler load-template}}]
+   ["/api/save-template" {:post {:handler save-template }}] ;(require-auth save-template) THAT IS IF I WANT CLERK
+   ["/api/load-template" {:post {:handler load-template}}]
    ;["/create-payment-intent" {:post {:handler (require-auth create-payment-intent)}}]
    ;["/create-subscription" {:post {:handler (require-auth create-subscription)}}]
-   ["/files/:file-id" {:get {:handler (require-auth serve-file)}}]
+   ["/api/files/:file-id" {:get {:handler serve-file}}]
    ;["/stripe-webhook" {:post {:handler stripe-webhook}}]
-   ["/clerk-webhook" {:post {:handler clerk-webhook}}]])
+   ["/api/clerk-webhook" {:post {:handler clerk-webhook}}]])
 
 (def app
   (ring/ring-handler
     (ring/router routes)
     (ring/routes
       (ring/create-default-handler
-        {:not-found (constantly {:status 404 :body "Not found"})
+        {:not-found          (constantly {:status 404 :body "Not found"})
          :method-not-allowed (constantly {:status 204})}))
     {:middleware [log-requests
                   #(wrap-cors % :access-control-allow-origin [#"https://www\.tabblio\.com"
                                                               #"https://tabblio\.com"
                                                               #"http://localhost:.*"]
-                                :access-control-allow-methods [:get :put :post :delete :options]
-                                :access-control-allow-headers ["Content-Type" "Authorization" "Accept" "x-clerk-session-token"])
+                              :access-control-allow-methods [:get :put :post :delete :options]
+                              :access-control-allow-headers ["Content-Type" "Authorization" "Accept" "x-clerk-session-token"]
+                              :access-control-allow-credentials true
+                              )
                   clerk/wrap-clerk-auth
                   wrap-json-body
                   wrap-json-response]}))
